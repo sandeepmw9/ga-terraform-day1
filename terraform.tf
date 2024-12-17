ee6acb1e121c6a2bc159335290eb30767f0a7cd5
sandeep@debian <sandeep@debian>
2024-12-17 12:07:53 +0530

    updated tf files

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,9 @@
 terraform {
-  # backend "s3" {
-  #   bucket = "tfstate1224"
-  #   key    = "terraform_states"
-  #   region = "ap-south-1"
-  # }
+  backend "s3" {
+    bucket = "tfstate1224"
+    key    = "terraform_states"
+    region = "ap-south-1"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
